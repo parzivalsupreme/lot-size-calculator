9b0a3e834909c06362f6d88bd5e5e03a1f05043c
Elijah Cezar <ajah35961@gmail.com>
2024-12-03 13:24:47 +0800

    .

--- lotSizeCalculator.cpp
+++ lotSizeCalculator.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
 int main()
 {
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    double risk, openPrice, stopLossPrice, accountBalance;
+    int contractSize = 1;
 
-    for (const string &word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "Open price: ";
+    cin >> openPrice;
+
+    cout << "Stop loss price: ";
+    cin >> stopLossPrice;
+
+    cout << "Account Balance: ";
+    cin >> accountBalance;
+
+    cout << "risk: ";
+    cin >> risk;
+
+    double riskAmount = accountBalance * risk / 100;
+
+    double priceDifference = openPrice - stopLossPrice;
+
+    double lotSize = riskAmount / priceDifference * contractSize;
+
+    cout << "Trade size: " << lotSize;
+
+    return 0;
 }
